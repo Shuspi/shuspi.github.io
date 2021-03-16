b73bf10086ca7b44bd95e76f6e8db228b7f729f9
Shuspi <80585553+Shuspi@users.noreply.github.com>
2021-03-16 07:49:05 +0400

    new

--- resources/scripts/background-randomizer.js
+++ resources/scripts/background-randomizer.js
@@ -2,7 +2,9 @@ var videoArray = [
   'resources/backgrounds/video-1.mp4',
   'resources/backgrounds/video-2.mp4',
   'resources/backgrounds/video-3.mp4',
-  'resources/backgrounds/video-4.mp4'
+  'resources/backgrounds/video-4.mp4',
+  'resources/backgrounds/video-5.mp4',
+  'resources/backgrounds/video-6.mp4'
 ];
 
 document.getElementById('background').src = videoArray[Math.floor(Math.random() * videoArray.length)];
